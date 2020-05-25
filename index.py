9b05a6afd497934fcf926e102dc8f935c44eda94
accounts_5e9ef74297567f00378d08e8@mail.teambition.com <accounts_5e9ef74297567f00378d08e8@mail.teambition.com>
2020-05-25 16:01:46 +0800

    更新 index.py

--- index.py
+++ index.py
@@ -5,11 +5,11 @@ import base64
 def handler(event, context):
   request = json.loads(event)
   logger = logging.getLogger()
-  content = "<h1>hello world</h1>".encode("utf-8")
+  content = "<h1>Hello world</h1><a href='list/200156'>打开 Gallery 列表</a>"
   api_rep = {
     "isBase64Encoded":"true",
     "statusCode":"200",
     "headers":{"Content-Type":"text/html; charset=utf-8"},
-    "body":str(base64.b64encode(content),"utf-8")
+    "body":str(base64.b64encode(content.encode("utf-8")),"utf-8")
   }
   return api_rep
