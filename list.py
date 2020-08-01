71d353dc95b381d65bb0a293c6e018064eee2dcb
accounts_5e9ef74297567f00378d08e8@mail.teambition.com <accounts_5e9ef74297567f00378d08e8@mail.teambition.com>
2020-08-02 00:45:12 +0800

    更新 list.py

--- list.py
+++ list.py
@@ -5,10 +5,7 @@ import base64
 def handler(event, context):
   request = json.loads(event)
   logger = logging.getLogger()
-  content = "<h1>Gallery 列表</h1>"
-  content += "<div>Gallery 列表的 ID 是："+request['pathParameters']['listId']+"</div>"
-  content += "<div>它从一个 Restful 的 API中解析得到 request['pathParameters']['listId']</div>"
-  content += "<a href='/'>回到首页</a>"
+  content = "<h1>Hello world</h1><a href='detail/200156'>看 Gallery 详情</a>"
   api_rep = {
     "isBase64Encoded":"true",
     "statusCode":"200",
